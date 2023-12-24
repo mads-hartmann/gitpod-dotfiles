41ad47f859c527030ccd42500809e79e566fc6c4
Mads Hartmann <mads379@gmail.com>
2023-12-24 21:16:58 +0100

    Add intro comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
+# My Gitpod specific dotfiles.
+#
+# See the official docs
+#   https://www.gitpod.io/docs/configure/user-settings/dotfiles
+#
+# For debugging
+#   cat /home/gitpod/.dotfiles.log
+#
+
 git config --global --add --bool push.autoSetupRemote true
 
 echo 'export EDITOR="code --wait"' >> $HOME/.bashrc.d/100-environment-variables
