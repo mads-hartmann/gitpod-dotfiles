9f6232e64196ea94654fcc342ca7ca659cf99bf3
Mads Hartmann <mads379@gmail.com>
2024-11-13 10:11:07 +0100

    An alternative git.tidy command

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ git config --global --add --bool push.autoSetupRemote true
 git config --global alias.b "branch"
 git config --global alias.st "status"
 git config --global alias.co "checkout"
-git config --global alias.tidy "!git branch --merged | grep -E -v '(master|staging)' | grep -E -v '^\\*' | xargs git branch -d"
+git config --global alias.tidy "!git branch | grep -E -v 'main' | xargs git branch -d"
 git config --global core.editor "code --wait"
 
 echo 'export EDITOR="code --wait"' >> $HOME/.profile
