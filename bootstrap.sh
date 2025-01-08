59ba0b1ee22b92bc5045881774d7493e5f3f8e0b
Mads Hartmann <mads379@gmail.com>
2025-01-08 10:20:40 +0100

    Add more aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 # These dotfiles are for Gitpod Flex
 git config --global --add --bool push.autoSetupRemote true
 git config --global alias.b "branch"
+git config --global alias.c "commit"
 git config --global alias.st "status"
+git config --global alias.s "switch"
 git config --global alias.co "checkout"
 git config --global alias.tidy "!git branch | grep -E -v 'main' | xargs git branch -d"
 git config --global core.editor "code --wait"
