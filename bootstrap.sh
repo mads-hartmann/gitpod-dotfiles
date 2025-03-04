3c703f5fb0933ae18a265b6c8a0d3e360f66ddf7
Mads Hartmann <mads379@gmail.com>
2025-03-04 12:45:27 +0000

    fixup: duplicate push.autoSetupRemote

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 # These dotfiles are for Gitpod Flex
-git config --global --replace-all --bool push.autoSetupRemote true
+git config --global --add --bool push.autoSetupRemote true
 git config --global alias.b "branch"
 git config --global alias.c "commit"
 git config --global alias.st "status"
@@ -7,7 +7,6 @@ git config --global alias.s "switch"
 git config --global alias.co "checkout"
 git config --global alias.tidy "!git branch | grep -E -v 'main' | xargs git branch -d"
 git config --global core.editor "cursor --wait"
-git config --global push.autoSetupRemote true
 
 echo 'export EDITOR="cursor --wait"' >> $HOME/.profile
 
